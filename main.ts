be07ec0c3877670e6723d9894932e157c1892370
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-03-25 13:12:37 -0400

    change and commited

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Murali Kothuri
+ * Created on: Mar 2026
+ * This program controls a servo.
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
